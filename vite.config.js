dc221b45bf911eab5a0e0b1454166e059517a44a
Ushikava <doni9791@gmail.com>
2026-05-31 07:06:23 +0300

    wedding_v2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wedding/',  // замени 'wedding' на название своего репозитория
+  base: '/wedding/',
   plugins: [react()],
   build: {
     rollupOptions: {
